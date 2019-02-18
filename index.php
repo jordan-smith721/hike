592688e67964f068acde50329a611255f48f2b41
Brooks Eshe <beshe@mail.greenriver.edu>
2019-02-18 13:23:40 -0500

    added validation to sign up form

--- index.php
+++ index.php
@@ -19,10 +19,97 @@ $f3 = Base::instance();
 //Turn on Fat-Free error reporting
 $f3->set('DEBUG', 3);
 
+//require validation page
+require_once('model/validation.php');
+
 //Define a default route
-$f3->route('GET /', function ()
+$f3->route('GET|POST /', function ($f3)
 {
+
+    //VALIDATE SIGN UP FORM
+        $isValid = false;
+
+        if(isset($_POST['fname']))
+        {
+            $fname = $_POST['fname'];
+            if(validName($fname))
+            {
+                $isValid = true;
+            }
+            else
+            {
+                $isValid = false;
+                $f3 -> set("errors['first']","Please enter a first name");
+            }
+        }
+
+        if(isset($_POST['lname']))
+        {
+            $lname = $_POST['lname'];
+            if(validName($lname))
+            {
+                $isValid = true;
+            }
+            else
+            {
+                $isValid = false;
+                $f3->set("errors['last']", "Please enter a last name");
+            }
+        }
+
+        if(isset($_POST['signUpEmail']))
+        {
+            $email = $_POST['signUpEmail'];
+            if(validEmail($email))
+            {
+                $isValid = true;
+            }
+            else
+            {
+                $isValid = false;
+                $f3->set("errors['email']","Please enter a valid email address");
+            }
+        }
+
+        if(isset($_POST['signPassword']))
+        {
+            $password = $_POST['signPassword'];
+            $confirm = $_POST['confirmPass'];
+
+            if(validPassword($password))
+            {
+                if($password != $confirm)
+                {
+                    $isValid = false;
+                    $f3 -> set("errors['confirmPass']","Passwords do not match.");
+                }
+                else{
+                    $isValid = true;
+                }
+            }
+            else
+            {
+                $isValid = false;
+                $f3->set("errors['password']", "Password must be 8 characters long.");
+            }
+        }
+
+        if($isValid)
+        {
+            $f3->reroute("/test");
+        }
+
+
+    //VALIDATE LOG IN INFORMATION IS IN DATABASE
+        //code will go here
+
     echo Template::instance()->render('views/home.html');
+});
+
+$f3->route('GET|POST /test', function ($f3)
+{
+    $template = new Template();
+    echo $template->render('views/testpage.html');
 });
 
 //Run fat free
