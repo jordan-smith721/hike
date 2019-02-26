8264ff3a79fc850fe4842a8a14cd5125845754e2
Jordan Smith <jordan.m.smith721@gmail.com>
2019-02-25 23:06:15 -0500

    Added client-side validation to sign-up modal

--- js/modal-js.js
+++ js/modal-js.js
@@ -1,44 +1,96 @@
+var isValid = false;
+
 $(document).ready(function()
 {
-    var formLogin = $('#loginForm');
-    var formSignup = $('#signupForm');
 
-    //login modal AJAX
-    $(formLogin).submit(function(event) {
-        //stop the form from submitting
-        event.preventDefault();
+    var fname = $('#fname');
+    var lname = $('#lname');
+    var signUpEmail = $('#signUpEmail');
+    var signUpPassword = $('#signPassword');
+    var confirmPass = $('#confirmPass');
+    var fnameError = $('#fnameError');
+    var lnameError = $('#lnameError');
+    var emailError = $('#emailError');
+    var passwordError = $('#passwordError');
+    var confirmError = $('#confirmError');
 
-        //serialize the form data
-        var formLoginData = $(this).serialize();
 
-        $.ajax({
-            type: 'POST',
-            url: "",
-            data: formLoginData,
-            success: function () {
-                alert("SUCCESS");
-            }
-        })
+    $(fname).on("blur", function ()
+    {
+        isValid = true;
+        fname.removeClass("inputBoxError");
+        fnameError.removeClass("d-block");
+        //if field is empty or contains anything besides words
+        if (fname.val() === "" || !(/^[a-zA-Z]+$/.test(fname.val())))
+        {
+            fname.addClass("inputBoxError");
+            fnameError.addClass("d-block");
+            isValid = false;
+        }
+    });
+
+    $(lname).on("blur", function ()
+    {
+        lname.removeClass("inputBoxError");
+        lnameError.removeClass("d-block");
+        //if field is empty or contains anything besides words
+        if (lname.val() === "" || !(/^[a-zA-Z]+$/.test(lname.val())))
+        {
+            lname.addClass("inputBoxError");
+            lnameError.addClass("d-block");
+            isValid = false;
+        }
+    });
+
+    $(signUpEmail).on("blur", function ()
+    {
+        signUpEmail.removeClass("inputBoxError");
+        emailError.removeClass("d-block");
+
+        //if field is not an email address make it an error
+        var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (regex.test(signUpEmail.val()) == false)
+        {
+            signUpEmail.addClass("inputBoxError");
+            emailError.addClass("d-block");
+            isValid = false;
+        }
+    });
+
+    $(signUpPassword).on("blur", function ()
+    {
+        signUpPassword.removeClass("inputBoxError");
+        passwordError.removeClass("d-block");
+
+        if(signUpPassword.val().length < 8)
+        {
+            signUpPassword.addClass("inputBoxError");
+            passwordError.addClass("d-block");
+            isValid = false;
+        }
 
     });
 
-    //signup modal AJAX
-    $(formSignup).submit(function(event) {
-        //stop the form from submitting
-        event.preventDefault();
+    $(confirmPass).on("blur", function ()
+    {
+        confirmPass.removeClass("inputBoxError");
+        passwordError.removeClass("d-block");
 
-        //serialize the form data
-        var formSignupData = $(this).serialize();
+        if(confirmPass.val() != signUpPassword.val())
+        {
+            confirmPass.addClass("inputBoxError");
+            confirmError.addClass("d-block");
+            isValid = false;
 
-        $.ajax({
-            type: 'POST',
-            url: "",
-            data: formSignupData,
-            success: function (data) {
-                alert("SUCCESS");
-            }
-        })
-
+        }
     });
 
+    var signupForm = $('#signupForm');
+    $(signupForm).on("submit", function()
+    {
+        if (!isValid)
+        {
+            event.preventDefault();
+        }
+    });
 });
